efab64038a936b146d63f88c6f7d0cbc1afeb39c
pasha <p.korenev@voroninstudio.eu>
2014-08-07 18:04:57 +0300

    articles controller

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,7 @@
+class ArticlesController < ApplicationController
+  def list
+  end
+
+  def item
+  end
+end
--- app/models/dynamic_router.rb
+++ app/models/dynamic_router.rb
@@ -1,22 +1,22 @@
 class DynamicRouter
   def self.load
     WarszawaHotel::Application.routes.draw do
-      scope "(:locale)" do
-        Page.all.each do |page|
-          #puts "Routing #{pg.name}"
-          to = 'pages#show'
-          if (page.controller && page.controller.length > 0) && (page.action && page.action.length > 0)
-            to = "#{page.controller}##{page.action}"
-          end
-
-          page.translations_by_locale.keys.each do |locale|
-            I18n.with_locale locale do
-              get page.path, :to => to, defaults: { page_id: page.id }
-            end
-          end
-
+      #scope "(:locale)" do
+      Page.all.each do |page|
+        #puts "Routing #{pg.name}"
+        to = 'pages#show'
+        if (page.controller && page.controller.length > 0) && (page.action && page.action.length > 0)
+          to = "#{page.controller}##{page.action}"
         end
+
+        page.translations_by_locale.keys.each do |locale|
+          I18n.with_locale locale do
+            get "(/#{locale.to_s})/page.path", :to => to, defaults: { page_id: page.id, locale: locale }
+          end
+        end
+
       end
+      #end
     end
 
     # if Configuration.settings_hash["detect_locale_automatically"] == 'true'
